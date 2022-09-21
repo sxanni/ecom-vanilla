2525a472bef9e4af96cfebc0350e8973e05e9a4c
sanni <opeyemisanni65@gmail.com>
2022-09-20 20:31:38 -0400

    4th commit :- Finished mobile responsive version of menus with js and css

--- js/main.js
+++ js/main.js
@@ -20,6 +20,30 @@ function copyMenu(){
 copyMenu();
 
 
+//show mobile menu
+const menuButton = document.querySelector('.trigger'),
+closeButton = document.querySelector('.t-close'),
+addclass = document.querySelector('.site');
+menuButton.addEventListener('click', function(){
+    addclass.classList.toggle('showmenu')
+})
+closeButton.addEventListener('click', function() {
+    addclass.classList.remove('showmenu')
+})
+
+
+//show sub menu on mobile
+const submenu = document.querySelectorAll('.has-child .icon-small');
+submenu.forEach((menu) => menu.addEventListener('click', toggle));
+
+function toggle(e) {
+    e.preventDefault();
+    submenu.forEach((item) => item != this ? item.closest('.has-child').classList.remove('expand') : null);
+    if (this.closest('.has-child').classList != 'expand');
+    this.closest('.has-child').classList.toggle('expand')
+}
+
+
 function myFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
